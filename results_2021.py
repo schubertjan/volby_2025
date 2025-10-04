17d95490fd2ded1f84b395b55c54bcb56aaa96be
jan-schubert <jan-schubert@external.telekom.de>
2025-10-04 14:02:10 +0200

    Init commit

---
+++ results_2021.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import json
+
+
+def PROP_MAE(y, pred, count):
+    return np.round(np.average(np.abs(y - pred), weights=count), 2)
+
+
+def mae(y, pred):
+    return np.round(np.mean(np.abs(y - pred)), 2)
+
+
+def create_url(elec_id, okres, obec, okrsek):
+    okres = str(int(okres))
+    obec = str(int(obec))
+    okrsek = str(int(okrsek))
+    return f"https://www.volby.cz/appdata/{elec_id}/vysled/okrsek/{okres}/{obec}_{okrsek}.json"
+
+
+project_dir = Path(__file__).parent
+okrsky_2021 = pd.read_csv(
+    project_dir / "volby_okrsky_2021.csv",
+    sep=",",
+    usecols=["OKRES", "OBEC", "OKRSEK", "KSTRANA", "POC_HLASU"],
+)
+
+okrsky_2021["OKRSEK_POC_HLASU"] = okrsky_2021.groupby(["OKRES", "OBEC", "OKRSEK"])[
+    "POC_HLASU"
+].transform("sum")
+okrsky_2021["OKRSEK_PRC"] = np.round(
+    100 * okrsky_2021["POC_HLASU"] / okrsky_2021["OKRSEK_POC_HLASU"], 2
+)
+
+celkovy_vysledek = np.round(
+    100
+    * okrsky_2021.groupby(["KSTRANA"], as_index=True)["POC_HLASU"].sum()
+    / okrsky_2021["POC_HLASU"].sum(),
+    2,
+).reset_index()
+celkovy_vysledek.columns = ["KSTRANA", "CR_PRC"]
+okrsky_2021 = pd.merge(okrsky_2021, celkovy_vysledek, on="KSTRANA", how="left")
+
+okrsky_mae = (
+    okrsky_2021.groupby(["OKRES", "OBEC", "OKRSEK"])
+    .apply(lambda x: PROP_MAE(x["OKRSEK_PRC"], x["CR_PRC"], x["POC_HLASU"]))
+    .reset_index()
+)
+okrsky_mae.columns = ["OKRES", "OBEC", "OKRSEK", "PROP_MAE"]
+okrsky_2021 = pd.merge(
+    okrsky_2021, okrsky_mae, on=["OKRES", "OBEC", "OKRSEK"], how="left"
+)
+
+results = okrsky_2021.sort_values(by=["PROP_MAE", "OKRSEK_POC_HLASU"], ascending=True)[
+    ["OKRES", "OBEC", "OKRSEK", "PROP_MAE", "OKRSEK_POC_HLASU"]
+].drop_duplicates()
+
+
+# based on threshold
+mae_threshold = 2.0
+results = results.loc[results["PROP_MAE"] < mae_threshold, :]
+# based on overall % of okrsek
+
+results["url_ps2021"] = results.apply(
+    lambda x: create_url("ps2021",x["OKRES"], x["OBEC"], x["OKRSEK"]), axis=1
+)
+results["url_ps2025"] = results.apply(
+    lambda x: create_url("ps2025",x["OKRES"], x["OBEC"], x["OKRSEK"]), axis=1
+)
+
+with open(project_dir / "results_2021.json", "w") as f:
+    json.dump(results.to_dict(orient="records"), f)
+results.to_csv(project_dir / "results_2021.csv", index=False)
---
+++ vysledky/prubezne_vysledky.md
@@ -0,0 +1,25 @@
+# 🗳️ Volební výsledky podle strany:
+
+Čas predikce:2025-10-04_13:50:54
+
+| NAZ_STRANA                     |   Procent hlasů |
+|:-------------------------------|----------------:|
+| ALIANCE NÁRODNÍCH SIL          |            0    |
+| ANO 2011                       |           26.87 |
+| Aliance pro budoucnost         |            0.12 |
+| Hnutí Prameny                  |            0.12 |
+| Komunistická str.Čech a Moravy |            3.51 |
+| Koruna Česká (monarch.strana)  |            0    |
+| Moravané                       |            0.6  |
+| Otevřeme ČR normálnímu životu  |            0.24 |
+| PIRÁTI a STAROSTOVÉ            |           15.37 |
+| PŘÍSAHA Roberta Šlachty        |            6.05 |
+| SENIOŘI 21                     |            0    |
+| SPOLU – ODS, KDU-ČSL, TOP 09   |           27.72 |
+| Strana zelených                |            1.69 |
+| Svoboda a př. demokracie (SPD) |            8.71 |
+| Trikolora Svobodní Soukromníci |            3.63 |
+| Urza.cz: Nechceme vaše hlasy   |            0    |
+| VOLNÝ blok                     |            1.33 |
+| Česká str.sociálně demokrat.   |            3.63 |
+| Švýcarská demokracie           |            0.36 |
